442b5b9a46d92f12f43421b1a2c850d0a91ea9ef
Daniel de Souza Lima <39025412+OHomemParede@users.noreply.github.com>
2020-07-11 12:59:18 -0300

    Update Recursividade.py

--- Recursividade.py
+++ Recursividade.py
@@ -1,14 +1,23 @@
 # FUNCOES RECURSIVAS EM PAITO
+"""sem usar dict ou CACHE e sem usar 2**n"""
+
+def f(n):
+    if n == 0: return 1
+    return f(n-1)*2 #f(n-1) + f(n-1)
+
 
 def inv(s): # inverte um string, recursivamente; o mesmo que s[::-1]
     if len(s) == 0: return s
 	return inv(s[1:])+s[0]
 
+
 def anagrama(s1, s2): #funcao anagrama
 	return sorted(s1) == sorted(s2)
 
+
 def anagrama(s1, s2): #minha versao do anagrama
 	return set(s1) == set(s2) and len(s1) == len(s2)
+	
 	
 def fat(n): #funcao fatorial
 	if n==1:return 1
@@ -19,13 +28,16 @@ def pot(a,b): #funcao potencia
 	if b == 1: return a
 	return a*pot(a,b-1)
 	
+	
 def sd(n): # soma os digitos de um numero
 	if n <= 9: return n
 	return n%10 + sd(n // 10)
 	
+	
 def mdc(a,b): #funcao mdc
 	if a%b == 0: return b
 	return mdc(a%b, b = a)
+	
 	
 def fib(n): #funcao fibonacci usando dict
 	f = {}
@@ -34,7 +46,6 @@ def fib(n): #funcao fibonacci usando dict
 		f[n] = fib(n-1)+fib(n-2)
 	return f[n]
 	
-
 
 #funcao fibonacci usando o cache do computador pra gardar os valores ja processados
 from functools import lru_cache
