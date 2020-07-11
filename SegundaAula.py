566f989dfbc9d96d54328c6907949286c8fb8836
Daniel de Souza Lima <39025412+OHomemParede@users.noreply.github.com>
2020-07-11 12:59:59 -0300

    Update SegundaAula.py

--- SegundaAula.py
+++ SegundaAula.py
@@ -1,13 +1,3 @@
-
-"""sem usar dict ou CACHE e sem usar 2**n"""
-
-def f(n):
-    if n == 0: return 1
-    return f(n-1)*2 #f(n-1) + f(n-1)
-
-print(f(10))
-print("_"*50)
-
 #__________________P O N T E I R O S__________________
 
 
@@ -16,16 +6,15 @@ lista2 = lista1
 lista1[0] = 42
 print(lista1)
 print(lista2)
-print("_"*50)
 
 
-""" uma copia e nao um ponteiro """
+print()
+print(""" uma copia e nao um ponteiro """)
 lista1 = [1, 2, 3]
 lista2=list(lista1)
 lista1[0] = 42
 print(lista1)
 print(lista2)
-print("_"*50)
 
 """
 Pyhton tem ponteiros, só que eu os uso em alto nível.
@@ -34,10 +23,10 @@ A memoria pode ser vista como um vetor
 gigantesco de bytes, cada índice é o endereço.
 Em C char ocupa um byte, int ocupa 4 bytes.
 
-Ponteiros em C acessa váriaveis através do seu endereço,
-ou regioes de memória, mesmo que não tenham variáveis declaradas
+Ponteiros em C acessa variáveis através do seu endereço,
+ou regiões de memória, mesmo que não tenham variáveis declaradas
 
-1) Ponteiro e coisa apontada são duas coisas diferenes
+1) Ponteiro e coisa apontada são duas coisas diferentes
 2) Não tem sentido usar um ponteiro que não tenha
 sido inicializado, isto é, que não aponte para ninguém
 
@@ -46,8 +35,6 @@ Em C ponteiros são usados para muitas coisas
 2) Posso declarar vetores com tamanho calculando em tempo de execução
 int *v;
 v pode significar duas coisas, um ponteiro para um único inteiro, ou um vetor
-que é alocado em tempo de execucão
-#em casa acesse bit.ly/fatecED
-#lá tem um link do dropbox do código em C
+que é alocado em tempo de execução
 
 """
